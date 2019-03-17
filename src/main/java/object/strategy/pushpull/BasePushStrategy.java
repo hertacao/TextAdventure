436df3e3cfa4551234f94e1be2f105ef8840e8ed
Herta <herta.cao@kiwigrid.com>
2019-03-17 23:27:54 +0100

    * transfer local data

--- src/main/java/object/strategy/pushpull/BasePushStrategy.java
+++ src/main/java/object/strategy/pushpull/BasePushStrategy.java
@@ -3,22 +3,33 @@ package object.strategy.pushpull;
 import command.BaseAction;
 import language.Article;
 import lombok.NonNull;
+import lombok.Setter;
 import process.Response;
 import util.AdvStringBuilder;
 import util.Word;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 public class BasePushStrategy implements PushStrategy{
-    private SwitchState switchState;
+    @Setter private SwitchState switchState;
     protected List<SwitchState> positions;
 
     public BasePushStrategy(String... positions) {
-        for (String pos: positions) {
-            SwitchState switchState = new SwitchState(pos);
-            this.positions.add(switchState);
+        List<String> pos = Arrays.asList(positions);
+        this.switchState = new SwitchState(this, pos.iterator().next());
+        SwitchState previousState = this.switchState;
+
+        while(pos.iterator().hasNext()) {
+            SwitchState nextState = new SwitchState(this, pos.iterator().next());
+            previousState.setNext(nextState);
+            this.positions.add(previousState);
+            previousState = nextState;
         }
+
+        previousState.setNext(this.switchState);
+        this.positions.add(previousState);
     }
 
     @Override
--- src/main/java/object/strategy/pushpull/SwitchState.java
+++ src/main/java/object/strategy/pushpull/SwitchState.java
@@ -2,17 +2,20 @@ package object.strategy.pushpull;
 
 import command.BaseAction;
 import language.Article;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.*;
 import process.Response;
 import util.AdvStringBuilder;
 import util.Word;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class SwitchState {
+    private final BasePushStrategy basePushStrategy;
+    @NonNull
     @Getter private String name;
+    @Setter private SwitchState next;
 
     public Response push(String label) {
+        this.basePushStrategy.setSwitchState(this.next);
         return new Response(true, AdvStringBuilder.buildSentence(Word.YOU, BaseAction.PUSH, Article.DEFINITE, label));
     }
 }
